2f017572f5208305d2140305c73dcafbb836c7ba
Gerardo Rivas Alvarez <windie@ciencias.unam.mx>
2022-11-03 16:24:40 -0600

    se agrego la funcion total_files

--- backup.sh
+++ backup.sh
@@ -6,6 +6,11 @@ user=$(whoami)
 input=/home/$user/Documents/scripting
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
+# La funcion total_files cuenta el numero de archivos en un directorio
+function total_files {
+	find $1 -type f | wc -l
+}
+
 tar -czf $output $input 2> /dev/null
 
 echo "Respaldo completado!"
