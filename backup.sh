90aeff2cd0776e6ac533b7d7dd9eb9d2d4d9d4a1
Gerardo Rivas Alvarez <windie@ciencias.unam.mx>
2022-11-03 15:57:13 -0600

    Se definieron variables de entrada y salida
    Se definio  una variable input con la direccion a respaldar,
    y una variable output con la direccion destino del comprimido con el respaldo.

--- backup.sh
+++ backup.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/bash
 
-tar -czf /tmp/myhome_directory.tar.gz /home/gerardo
+# Este script se utiliza para hacer un respaldo del directorio /home a /tmp
+# Para las pruebas se hace el backup de scripting
+user=$(whoami)
+input=/home/$user/Documentos/scripting
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+tar -czf $output $input 2> /dev/null
+
+echo "Respaldo completado!"
+
+echo "Detalles del archivo destino"
+ls -l $output
